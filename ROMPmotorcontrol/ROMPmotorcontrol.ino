59f63fccb0ba58527607d7f5a3ac2efd828cb69a
Murtaza Dalal <mdalal@berkeley.edu>
2017-03-22 16:43:28 -0700

    Added rotation functions

--- ROMPmotorcontrol/ROMPmotorcontrol.ino
+++ ROMPmotorcontrol/ROMPmotorcontrol.ino
@@ -9,12 +9,19 @@ Adafruit_DCMotor *internalRotationMotor = AFMS.getMotor(3);
 Adafruit_DCMotor *forearmRotationMotor = AFMS.getMotor(4);
 
 void setup() {
-  // put your setup code here, to run once
+  // put your setup code here, to run oncef
+  Serial.begin(9600);
   AFMS.begin();
   liftMotor1->setSpeed(100);
   liftMotor2->setSpeed(100);
   internalRotationMotor->setSpeed(100);
   forearmRotationMotor->setSpeed(100); 
+}
+void lift(int direction, int speed, int time);
+void rotateInternal(int direction, int speed, int time);
+void rotateForearm(int direction, int speed, int time);
+void loop() {
+  // put your main code here, to run repeatedly:
 }
 
 void lift(int direction, int speed, int time) {
@@ -35,7 +42,30 @@ void lift(int direction, int speed, int time) {
     liftMotor2->run(RELEASE);
   }
 }
-void loop() {
-  // put your main code here, to run repeatedly:
-  
+void rotateInternal(int direction, int speed, int time) {
+  internalRotationMotor->setSpeed(speed);
+  if(direction == 1) {
+    internalRotationMotor->run(FORWARD);
+    delay(time);
+    internalRotationMotor->run(RELEASE);
+  }
+  if(direction == -1) {
+    internalRotationMotor->run(BACKWARD);
+    delay(time);
+    internalRotationMotor->run(RELEASE);
+  }
 }
+void rotateForearm(int direction, int speed, int time) {
+  forearmRotationMotor->setSpeed(speed);
+  if(direction == 1) {
+    forearmRotationMotor->run(FORWARD);
+    delay(time);
+    forearmRotationMotor->run(RELEASE);
+  }
+  if(direction == -1) {
+    forearmRotationMotor->run(BACKWARD);
+    delay(time);
+    forearmRotationMotor->run(RELEASE);
+  }
+}
+
